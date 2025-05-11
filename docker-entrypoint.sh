3325785944bec67a89264533e048b9e0b3b69ead
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-05-10 21:21:01 -0600

    feat: reload added at saving changes in app

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,5 @@
 set -xe
 
 # Iniciar el servidor de FastAPI con Uvicorn usando Poetry
-exec uvicorn src.main:app --workers 1 --host 0.0.0.0 --port 80 --http h11
+exec uvicorn src.main:app --workers 1 --host 0.0.0.0 --port 80 --http h11 --reload
 
