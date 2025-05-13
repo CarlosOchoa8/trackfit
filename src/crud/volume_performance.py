46c02d401f432c58b4cb5e9ef30fdd7244e4b96e
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-05-12 20:32:19 -0600

    addL performance volume class

---
+++ src/crud/volume_performance.py
@@ -0,0 +1,7 @@
+from src.crud.performance_base_class import PerformanceCalculator
+
+class VolumePerformanceCalculator(PerformanceCalculator):
+    """Volume Performance Calculator class."""
+
+    def calculate_performance(self):
+        return "Volume Performance"
